972de057415ff5b73f3798507d13c08a7af4359b
usama <ukhalid639@gmail.com>
2023-09-28 11:23:59 +0500

    fox

--- script.js
+++ script.js
@@ -1,78 +1,116 @@
 var reference = document.getElementById("userscom-chat").getAttribute("data-reference");
-// var image = document.getElementById("userscom-chat").getAttribute("data-image");
-var image = "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png";
+var image = document.getElementById("userscom-chat").getAttribute("data-image");
+// var image = "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg";
 
 // Define the custom element tag
 function ChatBox() {
   // Create styles
   const styles = `
+  
+  @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
     /* Your styles go here */
     .open-button {
-      background-color: white;
+      background-color: #ffffff75;
       color: white;
-      padding: 16px;
+      padding: 7px;
       border: none;
       cursor: pointer;
-      opacity: 0.8;
-      position: fixed;
-      bottom: 23px;
-      right: 28px;
-      width: 100px;
+      width: 60px;
+      height: 60px;
       border-radius: 50%;
+      backdrop-filter: blur(8px);
     }
 
     /* The popup chat - hidden by default */
     .chat-popup {
-      display: none;
+      align-items: end;
+      display: flex;
       position: fixed;
-      bottom: 0;
+      
+      bottom: 15px;
       right: 15px;
-      z-index: 9;
+      z-index: 9999999;
+      flex-direction: column;
+      row-gap:1.125rem;
     }
 
     /* Add styles to the form container */
     .form-container {
-      max-width: 300px;
-      padding: 10px;
-      background-color: white;
+      display:none;
+      max-width: 400px;
+      width:350px;
+      border-radius:15px;
+      background: linear-gradient(180deg, #fff, #cad5e6);
+      overflow: hidden;
+    position: relative;
     }
 
+    /* Change placeholder text color for both input and textarea */
+    input::placeholder,
+    textarea::placeholder {
+      color: #94A3B8; /* Change this to the desired color */
+    }
+
+    
     /* Full-width textarea */
-    .form-container textarea {
+    textarea {
       width: 100%;
-      padding: 15px;
-      margin: 5px 0 22px 0;
+     
+      color:#64748B;
       border: none;
-      background: #f1f1f1;
+      font-family: 'Inter', sans-serif;
+      background: transparent;
+      
       resize: none;
-      min-height: 100px;
+      min-height: 200px;
+      padding:20px;
     }
-
+    
+    .textarea-wrapper{
+      border-bottom: 1px solid #bdcbe2;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+    }
+  
     .form-container input {
       width: 50%;
-      padding: 15px;
-      margin: 5px 0 22px 0;
-      border: none;
-      background: #f1f1f1;
+      padding-left: 20px;
+      padding-right: 20px;
+      font-family: 'Inter', sans-serif;
+      padding-top:10px;
+      padding-bottom:10px;
+      outline:transparent;
+      border: none; 
+      background: transparent;
       resize: none;
     }
 
     /* When the textarea gets focus, do something */
     .form-container textarea:focus {
-      background-color: #ddd;
+      background: transparent;
       outline: none;
     }
 
     /* Set a style for the submit/send button */
     .form-container .btn {
-      background-color: #04AA6D;
-      color: white;
-      padding: 16px 20px;
-      border: none;
-      cursor: pointer;
-      width: 100%;
-      margin-bottom: 10px;
-      opacity: 0.8;
+      background-color: #dfe6f3;
+    color: rgb(71, 89, 113);
+    padding: 16px 20px;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    width: -webkit-fill-available;
+    letter-spacing: -0.4px;
+    background-clip: padding-box;
+    margin: 7px;
+    border-radius: 9px;
+    transition: background-color 0.3s;
+
+
+    }
+   .btn:hover{
+      background-color:#ecf1f9;
     }
 
     /* Add a red background color to the cancel button */
@@ -84,9 +122,7 @@ function ChatBox() {
     }
 
     /* Add some hover effects to buttons */
-    .form-container .btn:hover, .open-button:hover {
-      opacity: 1;
-    }
+   
 
     #drop-area {
       // border-radius: 12px;
@@ -129,27 +165,30 @@ function ChatBox() {
     position: absolute;
     opacity: 0;
     cursor: pointer;
-    top: 80px;
+    top: 20px;
     right: 20px;
     width: 20px !important;
   }
 
   .attachmentContainer svg {
     width: 20px;
-    right: 30px;
+    right: 20px;
     position: absolute;
-    top: 90px;
+    top: 20px;
+    color:#bdcbe2;
   }
 
   #overlaySuccess {
+    
+   
     justify-content: center;
     align-items: center;
     position: absolute;
-    display: none;
+
     width: 100%;
     inset: 0px;
     transition: 0.3s;
-    background: linear-gradient(90deg, #90A4AE 0%, #CFD8DC 100%);
+    background: rgb(240, 255, 242);
     z-index: 2;
     cursor: pointer;
     height: 100%;
@@ -168,7 +207,12 @@ function ChatBox() {
     margin-bottom: 5px;
     text-align: center;
   }
+  #text {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 
+  }
   #successTextSubtitle
   {
     font-size: 14px;
@@ -177,7 +221,15 @@ function ChatBox() {
     margin-bottom: 20px;
     text-align: center
   }
+  .field-wrapper {
+    display:flex;
+    align-items:center;
+    border-bottom: 1px solid #bdcbe2 !important;
 
+  }
+  .field-wrapper > :first-child {
+    border-right: 1px solid #bdcbe2 !important;
+  }
   #successButton{
     border: solid 1px #8BB593;
     background-color: transparent;
@@ -217,10 +269,16 @@ function ChatBox() {
   form.className = "form-container";
 
   // Create textarea for message input
+  var fieldsWrapper = document.createElement("div");
+  fieldsWrapper.className="field-wrapper"
+
   var textarea = document.createElement("textarea");
   textarea.placeholder = "Type message..";
   textarea.name = "message";
   textarea.required = true;
+
+  var textAreaWrapper = document.createElement("div");
+  textAreaWrapper.className = "textarea-wrapper";
 
   var nameInput = document.createElement("input");
   nameInput.type = "text";
@@ -233,6 +291,10 @@ function ChatBox() {
   emailInput.placeholder = "Email";
   emailInput.name = "email";
   emailInput.required = true;
+  fieldsWrapper.appendChild(emailInput)
+  fieldsWrapper.appendChild(nameInput)
+  
+  textAreaWrapper.appendChild(textarea)
 
   // Drop Area
   const dropArea = document.createElement('div');
@@ -253,6 +315,7 @@ function ChatBox() {
     </svg>
   `;
 
+  form.appendChild(textAreaWrapper);
   // Append the input and label elements to the shadow root
   attachmentContainer.appendChild(inputFile);
   form.appendChild(attachmentContainer);
@@ -264,31 +327,36 @@ function ChatBox() {
   sendButton.textContent = "Send";
 
   // Create close button
-  var closeButton = document.createElement("img");
-  closeButton.src = image;
-  closeButton.className = "cancel";
+  // var closeButton = document.createElement("img");
+  // closeButton.src = image;
+  // closeButton.className = "cancel";
 
   // Append elements to form
-  form.appendChild(textarea);
+
   form.appendChild(dropArea);
-  form.appendChild(nameInput);
-  form.appendChild(emailInput);
+  form.appendChild(fieldsWrapper)
+
+  // form.appendChild(nameInput);
+  // form.appendChild(emailInput);
   form.appendChild(sendButton);
-  form.appendChild(closeButton);
+ 
+  // form.appendChild(closeButton);
 
   const overlaySuccessDiv = document.createElement('div');
   overlaySuccessDiv.id = 'overlaySuccess';
   const overlaySuccessDivText = document.createElement('div');
   overlaySuccessDivText.id = 'text';
 
-  var successIcon = document.createElement("div");
-  successIcon.className = "successIconContainer";
-    successIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-  </svg>
-  `;
+  overlaySuccessDiv.style.display="none";
 
-  overlaySuccessDivText.appendChild(successIcon);
+  // var successIcon = document.createElement("div");
+  // successIcon.className = "successIconContainer";
+  //   successIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  //   <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+  // </svg>
+  // `;
+
+  // overlaySuccessDivText.appendChild(successIcon);
 
   var successTextHeading = document.createElement("h6");
   successTextHeading.id = "successTextHeading";
@@ -306,13 +374,14 @@ function ChatBox() {
   overlaySuccessDivText.appendChild(successButton);
   
   overlaySuccessDiv.appendChild(overlaySuccessDivText);
-  chatPopup.appendChild(overlaySuccessDiv);
+  form.appendChild(overlaySuccessDiv);
 
   // Append form to chat popup
   chatPopup.appendChild(form);
+  chatPopup.appendChild(img)
 
   // Append chat button and chat popup to the body
-  userscomRoot.appendChild(img);
+  // userscomRoot.appendChild(img);
   userscomRoot.appendChild(chatPopup);
   // userscomRoot.appendChild(parentDiv);
   // document.body.appendChild(img);
@@ -320,19 +389,26 @@ function ChatBox() {
 
   // Add event listeners
   img.addEventListener("click", function () {
-    chatPopup.style.display = "block";
-  });
-
-  closeButton.addEventListener("click", function () {
-    chatPopup.style.display = "none";
-  });
-
-  var successButton = chatPopup.querySelector("#successButton");
-  successButton.addEventListener("click", function () {
-    var overlayDivSuccess = chatPopup.querySelector("#overlaySuccess");
-    if (overlayDivSuccess) {
-      overlayDivSuccess.style.display = "none";
+    
+    if(form.style.display==='block'){
+      form.style.display = "none";
+    }else{
+      form.style.display = "block";
     }
+  });
+
+  // img.addEventListener("click", function () {
+  //   form.style.display = "none";
+  // });
+
+  var successButton = form.querySelector("#successButton");
+  successButton.addEventListener("click", function () {
+    // var overlayDivSuccess = chatPopup.querySelector("#overlaySuccess");
+    overlaySuccessDiv.style.display="none";
+    form.querySelector("#overlaySuccess").style.display = "none";
+  //  form.querySelector("#overlaySuccess").style.display = "none";
+
+    
   });
 
   let userAttributes = {};
@@ -354,7 +430,7 @@ function ChatBox() {
             method: 'POST',
             body: formData,
         }).then((response) => {
-          chatPopup.querySelector("#overlaySuccess").style.display = "flex";
+          form.querySelector("#overlaySuccess").style.display = "flex";
         })
   });
 
